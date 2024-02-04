c7cae106ff34b8a0892ab028e02448465fbd1518
baragu <baraguantonyy@gmail.com>
2024-02-04 20:41:52 +0300

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
+
 # parse logfile and output [sender],[receiver],[flags]
 
-puts ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+# Check if the command line argument is provided
+if ARGV.empty?
+  puts 'Usage: ./100-textme.rb <log_entry>'
+  exit
+end
+
+# Extract relevant information from the log entry
+result = ARGV[0].scan(/\[(?:from:|to:|flags:)(.*?)\]/).join(",")
+
+# Output the result
+puts result
+
