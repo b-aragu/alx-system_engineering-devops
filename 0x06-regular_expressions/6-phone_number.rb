e452e2f68f40bce24459eaef9cb9a73eef7c7958
baragu <baraguantonyy@gmail.com>
2024-02-26 07:05:45 +0300

    changes

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-# A script that checks a 10 numbers exactly
-
-def match_word(arg)
-  regex = /\b\d{10}\b/
-  matches = arg.scan(regex)
-  result = matches.join
-  puts result
-end
-
-# check commandline arguments
-
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <argument>"
-else
-  argument = ARGV[0]
-  match_word(argument)
-end
+puts ARGV[0].scan(/^\d{10}$/).join
