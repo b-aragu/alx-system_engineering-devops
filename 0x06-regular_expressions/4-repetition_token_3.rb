646ad7c45721a3e1050dbb55acb871af267e9056
baragu <baraguantonyy@gmail.com>
2024-02-26 07:01:08 +0300

    repetition

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt*n/).join
+# A script that checks a word repettion
+
+def match_word(arg)
+  regex = /hbt*n/
+  matches = arg.scan(regex)
+  result = matches.join
+  puts result
+end
+
+# check commandline arguments
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <argument>"
+else
+  argument = ARGV[0]
+  match_word(argument)
+end
