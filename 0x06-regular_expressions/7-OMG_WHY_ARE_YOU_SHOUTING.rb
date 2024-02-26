812b22c9285ecd779aa8b12e1a6e7b36d0f0fb90
baragu <baraguantonyy@gmail.com>
2024-02-26 07:03:57 +0300

    omg

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,18 @@
 #!/usr/bin/env ruby
-# Only match capital letters
+# A script that checks a 10 numbers exactly
 
-puts ARGV[0].scan(/[A-Z]/).join
+def match_word(arg)
+  regex = /[A-Z]/
+  matches = arg.scan(regex)
+  result = matches.join
+  puts result
+end
+
+# check commandline arguments
+
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <argument>"
+else
+  argument = ARGV[0]
+  match_word(argument)
+end
